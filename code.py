4bef64e11aeb7e6bd5e385e0461d4dd76ba3f59e
Shyaam Menon <110072515+Injector287@users.noreply.github.com>
2023-08-03 16:51:42 +0530

    Added main code and the credential.dat
    Fixed the newid()

---
+++ code.py
@@ -0,0 +1,21 @@
+import pickle as b
+
+def newid():
+    with open("D:\\Shyaam\\Project\\The_Ultimate_Project\\Credential.dat","rb") as cred:
+            cred.seek(0)
+            r=b.load(cred)
+            print(r)
+    '''nus=input("Enter New UserID:")
+    npas=input("Enter New Password:")
+    rpas=input("Re-enter Password:")
+    if npas==rpas:
+        with open("D:\\Shyaam\\Project\\The_Ultimate_Project\\Credential.dat","rb") as cred:
+            cred.seek(0)
+            r=b.load(cred)
+            r[nus]=npas
+        with open("D:\\Shyaam\\Project\\The_Ultimate_Project\\Credential.dat","wb") as cred:
+            cred.seek(0)
+            b.dump(r,cred)'''
+
+                   
+newid()
