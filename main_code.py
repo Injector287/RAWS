11d0875c467e6f30e9a7919e279b0a69705d38d2
Injector287 <shyaammenon.2017@gmail.com>
2023-08-06 08:11:52 +0530

    Created a new *login* function. Decluttered code.

--- main_code.py
+++ main_code.py
@@ -1,17 +1,39 @@
 import pickle as b
+from getpass import getpass
+
+# PART-1 Login and User Creation
 
 def newid():
+    print("Creating New User...")
     nus=input("Enter New UserID:")
-    npas=input("Enter New Password:")
-    rpas=input("Re-enter Password:")
+    npas=getpass("Enter New Password:")
+    rpas=getpass("Re-enter Password:")
     if npas==rpas:
-        with open("D:\\Shyaam\\Project\\The_Ultimate_Project\\Credential.dat","rb") as cred:
+        print(npas,rpas)
+        with open("Credential.dat","rb") as cred:
             cred.seek(0)
             r=b.load(cred)
             r[nus]=npas
-        with open("D:\\Shyaam\\Project\\The_Ultimate_Project\\Credential.dat","wb") as cred:
+        with open("Credential.dat","wb") as cred:
             cred.seek(0)
             b.dump(r,cred)
 
-                   
-newid()
+def login():
+    print("Login")
+    print("Enter 'new' for New User")
+    usn=input("Enter UserID:")
+    if usn=='new':
+        newid()
+    pas=getpass("Enter Password:")
+    with open("Credential.dat","rb") as cred:
+            cred.seek(0)
+            r=b.load(cred)
+    try:
+        if r[usn]==pas:
+            print('Access Granted')
+    except KeyError:
+        print("UserID not Found")
+    else:
+        print("Password Incorrect")
+
+login()
