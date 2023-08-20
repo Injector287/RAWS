ddd0371c21b3809d090ee3087817a53c759d8581
Injector287 <shyaammenon.2017@gmail.com>
2023-08-20 10:58:13 +0530

    Added Database, Table adder functions

--- main_code.py
+++ main_code.py
@@ -1,7 +1,8 @@
 import pickle as b
 from getpass import getpass
+import mysql.connector as m
 
-# PART-1 User Creation and Login
+#! PART-1 User Creation and Login
 
 def newid():
     print("Creating New User...")
@@ -35,5 +36,22 @@ def login():
         print("UserID not Found")
     else:
         print("Password Incorrect")
+#login()
 
-login()
+#! PART-2
+
+mydb=m.connect(host="localhost",user="root",password="root")
+cur=mydb.cursor()
+
+def opndb():
+    cur.execute("create database if not exists ALMS;")
+    cur.execute("use ALMS")
+    cur.execute("create table if not exists AirDet(PlaneID char(10) NOT NULL,Name varchar(50),Date date,DepTime time,DeptLoc varchar(15),ArrTime time,ArrLoc varchar(15),Primary Key(PlaneID)) ")
+    cur.execute("create table if not exists PackDet(PackageID char(10) NOT NULL,PlaneID char(10) NOT NULL,Type varchar(50),Weight int,Priorty char(1),Date date,Location varchar(15),Cost int,Primary Key(PackageID)) ")
+    cur.execute("create table if not exists CusDet(CustomerID char(10) NOT NULL,Name varchar(50),PackageID varchar(10) NOT NULL,Address varchar(15),Primary Key(CustomerID)) ")
+    mydb.commit()
+
+def inp():
+    pass
+
+opndb()
